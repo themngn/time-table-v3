32881e5cb7a85c06406ffe4d1d2cc4167a9a2277
Maksym Dukhota <monogon.ua@gmail.com>
2024-08-25 12:10:46 +0300

    Added Vue

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/admin">Admin</RouterLink>
+        </nav>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped>
+header {
+    line-height: 2;
+}
+
+nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+}
+
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+</style>
---
+++ src/views/AdminView.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="about">
+        <h1>This is an admin page</h1>
+        <ul v-for="group in groups" :key="group._id">
+            <li @click="fetchTimetable(group._id)">{{ group.name }}</li>
+        </ul>
+        <br />
+        <ul v-for="lesson in timetable" :key="lesson._id">
+            <li>
+                {{ lesson.name }}
+                <ul v-for="cclass in lesson.classes" :key="cclass._id">
+                    <li>
+                        {{ cclass.day_of_week }} {{ cclass.start_time }}
+                        {{ cclass.end_time }} {{ cclass.type }}
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AdminView",
+    components: {},
+    data() {
+        return {
+            groups: [],
+            timetable: [],
+        };
+    },
+    methods: {
+        async fetchGroups() {
+            const response = await fetch("http://localhost:3000/api/groups", {
+                methods: "GET",
+            });
+            this.groups = await response.json();
+        },
+        async fetchTimetable(id) {
+            const response = await fetch(
+                `http://localhost:3000/api/groups/${id}/timetable`
+            );
+            this.timetable = await response.json();
+            console.log(this.timetable);
+        },
+    },
+    mounted() {
+        this.fetchGroups();
+    },
+};
+</script>
+
+<style>
+.about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+</style>
